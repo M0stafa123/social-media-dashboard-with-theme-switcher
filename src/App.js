dc95311838c56fdc362b375c549af4e08c6ffe5a
kirito0san <jodat.ahmedsyed@gmail.com>
2024-01-16 21:33:11 +0200

    set app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-
 import Overview from "./components/Overview";
 
 function App() {
   return (
     <div>
       <Overview />
-
-import Header from "./components/Header";
-function App() {
-  return (
-    <div>
-      <Header />
-
     </div>
   );
 }

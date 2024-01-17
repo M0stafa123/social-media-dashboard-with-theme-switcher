afb3d238fd2376eb91bc228d3de8c01945a713f7
kirito0san <jodat.ahmedsyed@gmail.com>
2024-01-17 02:35:12 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         VeryDark_bg: "hsl(228, 28%, 20%)",
       },
       backgroundImage: {
+        insta: "linear-gradient(to right,hsl(37, 97%, 70%),hsl(329, 70%, 58%))",
         toggle: "linear-gradient(to right,hsl(210, 78%, 56%),hsl(146, 68%, 55%))   ",
       },
       fontFamily: {
